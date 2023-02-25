ddf5dccf854cec77135abf7a582063f92e9ea0d4
nitinarora <nitin.arora@solicon.co.in>
2023-02-25 15:22:46 +0530

    testing

---
+++ bashscript.sh
@@ -0,0 +1,2 @@
+Normal-Script Shell#!/bin/bashecho -e "Nitin Arora \e[94m\e[5m\e[4mCloud & DevOps\e[0m"echo -e "\n"echo -e "\e[2m\e[96mALL INFORMATION OF CURRENT SERVER\n"echo "today date"date;
+echo -e "\e[31mcurrent user"last -aecho -e "\e[96m><><><><><><><><><><><><><><>\n"echo "CPU && RAM Utillization Information/n"df-h |xargs|awk '{print "Free /total disk : "$11" / "$9""\n}'free -m \xargs|awk '{print "Free /total memory "$10" / "$8" "}'echo -e "\e[46mUtillaztion & Most Expensive Process\n"echo "Check TCP"cat /proc/net/tcp |wc -lastecho -e "/e[1mkernal name"uname -r
